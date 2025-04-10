16ba9d10118d31bb0c1a49f8f066f5138649cdf3
HeeDragoN1123 <yhy7952@gmail.com>
2025-04-10 21:47:36 +0900

    문제2번 ì¶í풀이

--- src/No_2.java
+++ src/No_2.java
@@ -3,24 +3,49 @@ import java.util.Scanner;
 public class No_2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int a = 170;
-        int b = 70;
-        System.out.println((double)b / 2.8899999999999997);
+        
+        //문제 스캐너로 키(cm)와 몸무게kg을 입력받는다.
+        //bmi지수를 구하여 비만,과체중,정상,저체중으로 판정하여 출력한다.
+
+// 입력 예시
+//        키 = 170
+//        몸무게 = 70
+
+//          출력 예시
+//        키(cm) : 170
+//        몸무게(Kg) :70
+//        24.221451928410353(과체중)
+        
+        
+
+        // 키 ,  몸무게는 정수이기 때문에  int값으로 변수를 지정
+        int height,weight;
+
+        // BMI 지수는  몸무게 / 키(m) *키(m) 를 나타내는 소수점으로 내려가는 수치 이기 때문에 double 로 받음
+        double BMI;
+
+        // 스캐너 로 키와 몸무게를 입력 받음
         System.out.print("키 = ");
-        int height = input.nextInt();
+        height = input.nextInt();
         System.out.print("몸무게 = ");
-        int weight = input.nextInt();
-        System.out.print("BMI = ");
-        double BMI = input.nextDouble();
-        System.out.println("키(cm) : " + height + "  몸무게(Kg) :" + weight);
-        if (BMI >= (double)25.0F) {
-            System.out.println("비만입니다.");
-        } else if ((double)25.0F > BMI && BMI >= (double)23.0F) {
-            System.out.println("과체중입니다.");
-        } else if ((double)23.0F > BMI && BMI >= (double)18.5F) {
-            System.out.println("정상입니다.");
+        weight = input.nextInt();
+
+        //BMI 계산  100.0을 넣거나 foalt 를 뜻하는 f를 100 뒤에다가 넣어야 에러가 나지 않는다.
+        // 만약 100으로 실행을 한다면 뒤에 소수점이 나오지 않을것이다.
+        double heightM = height / 100.f;
+        BMI = weight / (heightM * heightM);
+
+
+        // BMI판정
+        if (BMI >=25) {
+            System.out.println("키(cm) : " + height +"\n" +  "몸무게(Kg) :" + weight +"\n" + BMI + "(비만)");
+
+        } else if (25.0 > BMI && BMI >= 23) {
+            System.out.println("키(cm) : " + height +"\n" +  "몸무게(Kg) :" + weight +"\n" + BMI + "(과체중)");
+        } else if (23 > BMI && BMI >= 18.5) {
+            System.out.println("키(cm) : " + height +"\n" +  "몸무게(Kg) :" + weight +"\n" + BMI + "(정상)");
         } else {
-            System.out.println("저체중입니다.");
+            System.out.println("키(cm) : " + height +"\n" +  "몸무게(Kg) :" + weight +"\n" + BMI + "(저체중)");
         }
 
     }

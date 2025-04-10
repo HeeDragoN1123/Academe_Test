88edcb9d17f78fe9d41d7feb265bacf04d7d91bf
HeeDragoN1123 <yhy7952@gmail.com>
2025-04-10 22:23:05 +0900

    No_8.java 구구단 코드 수정

--- src/No_8.java
+++ src/No_8.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class No_8 {
+    public static void main(String[] args){
+
+        //변수 지정
+        int a,b;
+
+        // 반복문으로 a 1 에서 9까지 돌림
+        // a가 1번 돌아갈때마다 b가 1에서 9까지 돌아가며 곱셈을 하게됨
+        for(a =1; a <=9; a++){
+            for(b = 1; b<=9; b++){
+
+                System.out.print(a +"*" + b  +"=" + a*b + "\t");
+            }
+            System.out.println();
+        }
+
+    }
 }

89b9e847f13392721b8af6867347956f69409875
HeeDragoN1123 <yhy7952@gmail.com>
2025-04-10 22:28:46 +0900

    문제6번 해설

--- src/No_6.java
+++ src/No_6.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class No_6 {
+
+    public static void main(String[] args){
+
+        //단수 넣을 스캐너 생성
+        Scanner input = new Scanner(System.in);
+
+        // 변수 지정
+        int num;
+
+        // 스캐너로 변수 num를 입력 받음
+        System.out.print("단 입력 : ");
+        num = input.nextInt();
+
+
+        for(int i=1; i<=9; i++){
+
+            System.out.println(num +  "*"  + i + "="  + num* i  );
+        }
+
+
+
+    }
 }

43fd9a64824cbc2e25069b1330f93cc44f8a8291
HEE <hee@HEEui-MacBookPro.local>
2025-04-10 21:09:48 +0900

    맥북에서 윈도우로

--- src/No_2.java
+++ src/No_2.java
@@ -1,6 +1,27 @@
-public class No_2 {
-    public static void main(String[] args){
+import java.util.Scanner;
 
-        System.out.println("hello world");
+public class No_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = 170;
+        int b = 70;
+        System.out.println((double)b / 2.8899999999999997);
+        System.out.print("키 = ");
+        int height = input.nextInt();
+        System.out.print("몸무게 = ");
+        int weight = input.nextInt();
+        System.out.print("BMI = ");
+        double BMI = input.nextDouble();
+        System.out.println("키(cm) : " + height + "  몸무게(Kg) :" + weight);
+        if (BMI >= (double)25.0F) {
+            System.out.println("비만입니다.");
+        } else if ((double)25.0F > BMI && BMI >= (double)23.0F) {
+            System.out.println("과체중입니다.");
+        } else if ((double)23.0F > BMI && BMI >= (double)18.5F) {
+            System.out.println("정상입니다.");
+        } else {
+            System.out.println("저체중입니다.");
+        }
+
     }
 }
